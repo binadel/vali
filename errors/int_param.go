6faa668010603b166980a8707fabc65629a0f306
Hamid Asgari <hamidasgari313@gmail.com>
2025-11-17 06:21:09 +0330

    feat: create an error with an extra integer field

---
+++ errors/int_param.go
@@ -0,0 +1,26 @@
+package errors
+
+import "github.com/mailru/easyjson/jwriter"
+
+type IntParamError struct {
+	BasicError
+	ParamName  string
+	ParamValue int64
+}
+
+func (e *IntParamError) Error() string {
+	return e.Message
+}
+
+func (e *IntParamError) MarshalEasyJSON(w *jwriter.Writer) {
+	w.RawByte('{')
+	w.RawString(keyCode)
+	w.String(string(e.Code))
+	w.RawString(keyMessage)
+	w.String(e.Message)
+	w.RawString(",\"")
+	w.RawString(e.ParamName)
+	w.RawString("\":")
+	w.Int64(e.ParamValue)
+	w.RawByte('}')
+}
